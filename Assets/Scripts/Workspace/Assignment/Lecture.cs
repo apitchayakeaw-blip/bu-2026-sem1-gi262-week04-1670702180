7cb9fab4bce3fabe72af16fb82192f9438fe944c
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2026-09-08 15:59:58 +0700

    .

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -10,7 +10,7 @@ namespace Assignment
             // LCT01_SyntaxList();
             // LCT02_SyntaxLinkedList();
             // LCT03_SyntaxHashTable();
-            // LCT04_SyntaxDictionary();
+             LCT04_SyntaxDictionary();
         }
 
         #region Lecture
@@ -22,7 +22,42 @@ namespace Assignment
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            LinkedList<string> linkedList = new LinkedList<string>();
+            linkedList.AddLast("Node 1");
+            linkedList.AddLast("Node 2");
+            linkedList.AddFirst("Node 0");
+            PrintLinkList(linkedList);
+
+            LinkedListNode<string> frisNode = linkedList.First;
+            Debug.Log("first: " + frisNode.Value);
+            LinkedListNode<string> lastNode = linkedList.Last;
+            Debug.Log("last: " + lastNode.Value);
+            LinkedListNode<string> node1 = linkedList.Find("Node 1");
+            Debug.Log("node: " + node1.Value);
+            Debug.Log(node1.Previous.Value);
+            Debug.Log(node1.Next.Value);
+            if (frisNode.Previous == null) { Debug.Log("firstNode.Previous is null"); }
+            if (lastNode.Previous == null) { Debug.Log("lastNode.Next is null"); }
+
+            linkedList.AddAfter(node1, "Node 1.5");
+            linkedList.AddBefore(node1, "Node 0.5");
+            PrintLinkList(linkedList);
+
+            linkedList.RemoveFirst();
+            PrintLinkList(linkedList);
+            linkedList.Remove("Node 2");
+            PrintLinkList(linkedList);
+            linkedList.Clear();
+            PrintLinkList(linkedList);
+        }
+
+        void PrintLinkList(LinkedList<string> linkedList)
+        {
+            Debug.Log("----- linkedList -----");
+            foreach (string s in linkedList)
+            {
+                Debug.Log(s);
+            }
         }
 
         public void LCT03_SyntaxHashTable()
@@ -32,7 +67,40 @@ namespace Assignment
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
+            Dictionary<int , string> dic = new Dictionary<int , string>();
+            dic.Add(1, " Apple");
+            dic.Add(2, " Banana");
+            dic.Add(3, " Cherry");
+
+            int keytocheck = 1;
+            bool hasKey = dic.ContainsKey(keytocheck);
+            Debug.Log($" has key {keytocheck} : {hasKey}");
+            if ( hasKey )
+            {
+                Debug.Log(dic[keytocheck]);
+            }
+
+            foreach ( int key in dic.Keys )
+            {
+                Debug.Log(key);
+            }
+            foreach (string s  in dic.Values )
+            {
+                Debug.Log(s);
+            }
+
+            dic.Remove(1);
+            foreach (string s in dic.Values)
+            {
+                Debug.Log(s);
+            }
+
+            dic.Clear();
+            foreach (string s in dic.Values)
+            {
+                Debug.Log(s);
+            }
+
         }
 
         #endregion
